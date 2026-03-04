c2c6d98fef434d10547685ca7837db09d45c1afa
Samuel Verbel <avilezverbel13@gmail.com>
2026-03-03 19:31:45 -0500

    Remove dotenv and use static config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // [IMPORTANTE]
+import 'core/config/env.dart';
 
 import 'core/theme/app_theme.dart';
 import 'core/routes/app_routes.dart';
@@ -11,16 +11,10 @@ void main() async {
   // Asegura que el motor de Flutter esté listo antes de llamar código nativo
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 1. Carga las variables de entorno desde el archivo .env
-  //    Si el archivo no existe (ej. olvidaste crearlo), lanzará error.
-  await dotenv.load(fileName: ".env");
-
   // 2. Inicializa Supabase usando las variables seguras
   await Supabase.initialize(
-    // dotenv.env['NOMBRE_VAR'] lee el string del archivo .env
-    // Si no encuentra la variable, usamos '' para evitar crash inmediato (aunque fallará la conexión)
-    url: dotenv.env['SUPABASE_URL'] ?? '',
-    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '',
+    url: Env.supabaseUrl,
+    anonKey: Env.supabaseAnonKey,
   );
 
   runApp(
